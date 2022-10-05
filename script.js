01adcae94e37a5a5c50615fa9a588ef7b1e98776
Ahammed Shaheedhudheen <shaheedhudheenpambalath@gmail.com>
2022-10-05 14:06:52 +0530

    working on js

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
-const gameBoard = {
-    
-}
+const gameBoard = (() => {
+  let board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+
+  function addMarks(index, mark) {
+    board[index] = mark;
+  }
+
+  function getMark(index){
+    return board[index]
+  }
+
+  return { addMarks, getMark };
+})();
+
+const players = () => {
+
+   ``
+};
+
+//display controller
+const displayController = (() => {
+  const boxContainer = document.querySelector(".container");
+  const boxes = document.querySelectorAll(".box");
+  
+
+  //insert value of array to each box
+  boxes.forEach((box, index) => {
+    box.addEventListener(
+      "click",
+      (e) => {
+        const index = box.getAttribute("data-index");
+        gameBoard.addMarks(index, "X");
+      },
+      { once: true }
+    );
+  });
+
+  return {  };
+})();
+
+const gameController = (() => {
+  
+  let currentPlayer = players.playerX
+
+  function switchPlayer(currentPlayer) {
+    if(currentPlayer = playerX){
+      currentPlayer = players.playerO
+    } else{
+      return
+    }
+  }
+
+  return { switchPlayer };
+})();
