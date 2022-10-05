d57a915384e35c559f9d88852462a312422e1e2a
Ahammed Shaheedhudheen <shaheedhudheenpambalath@gmail.com>
2022-10-05 23:15:47 +0530

    new changes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const Players = (name, sign) => {
 };
 
 const gameBoard = (() => {
-  let gameboard = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
 
   function setBoard(index, sign) {
     gameboard[index] = sign;
@@ -19,9 +19,20 @@ const gameBoard = (() => {
   return { getBoard, setBoard, gameboard };
 })();
 
-const gameController = (()=> {
+const gameController = (() => {
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
   
-})()
+})();
 
 const displayController = (() => {
   let boxes = document.querySelectorAll(".box");
@@ -34,27 +45,26 @@ const displayController = (() => {
   let currentPlayer = playerX;
 
   function switchPlayers() {
-     if (currentPlayer.getSign() === playerX.getSign()) {
+    if (currentPlayer.getSign() === playerX.getSign()) {
       currentPlayer = playerO;
-     } else {
-       currentPlayer = playerX;
-     }
+    } else {
+      currentPlayer = playerX;
+    }
   }
 
   function addMarker() {
     boxes.forEach((box) => {
-
       box.addEventListener(
         "click",
         (e) => {
           let index = box.getAttribute("data-index");
           console.log(currentPlayer.getSign());
-          if ((currentPlayer.getSign() === playerX.getSign())) {
-            gameBoard.setBoard(index, playerX.getSign())
+          if (currentPlayer.getSign() === playerX.getSign()) {
+            gameBoard.setBoard(index, playerX.getSign());
           } else {
             gameBoard.setBoard(index, playerO.getSign());
           }
-          box.textContent = gameBoard.getBoard(index)
+          box.textContent = gameBoard.getBoard(index);
           switchPlayers();
         },
         { once: true }
@@ -64,8 +74,5 @@ const displayController = (() => {
 
   return { addMarker };
 })();
-
-
-
 
 displayController.addMarker();
